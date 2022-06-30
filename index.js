82e8525fc37cd782a64a9f41eb950a91a750673d
michaelsatumba <mjsatumba@gmail.com>
2022-06-30 14:47:06 -0700

    center button & copyright date

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 function random() {
+	var x = Math.floor(Math.random() * 55) + 1;
 
-
-var x = Math.floor(Math.random() * 55) + 1;
-
-
-$(".imageClass").attr("src", "images/" + x);
-
+	$('.imageClass').attr('src', 'images/' + x);
 }
